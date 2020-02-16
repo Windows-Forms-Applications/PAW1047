623604bf6931c8dd94a50a5b1b77496da1ba3e12
Cristian Ciurea <cristianciurea@gmail.com>
2020-02-16 18:41:03 +0200

    update

---
+++ sem1_paw/sem1_paw/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sem1_paw
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            Console.WriteLine("Introduceti numele!");
+            string nume = Console.ReadLine();
+
+            Console.Write("Numele este {0}", nume);
+        }
+    }
+}
