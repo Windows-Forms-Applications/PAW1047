d64f9e066b6b51cc6b00a6a29a216170c8fc7650
Cristian Ciurea <cristianciurea@gmail.com>
2020-02-17 18:06:53 +0200

    update

---
+++ sem1_paw/sem1_paw/Student.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sem1_paw
+{
+    class Student
+    {
+        int cod;
+        private string nume;
+        public int varsta;
+        protected float medie;
+
+        public Student()
+        {
+            cod = 0;
+            nume = "Anonim";
+            varsta = 0;
+            medie = 0.0f;
+        }
+
+        public Student(int c, string n, int v, float m)
+        {
+            cod = c;
+            nume = n;
+            varsta = v;
+            medie = m;
+        }
+
+        public Student(Student s)
+        {
+            this.cod = s.cod;
+            this.nume = s.nume;
+            this.varsta = s.varsta;
+            this.medie = s.medie;
+
+        }
+
+        public void afisare()
+        {
+            Console.WriteLine("Studentul {0} are codul {1}, varsta {2} si media {3}",
+                nume, cod, varsta, medie);
+        }
+    }
+}
